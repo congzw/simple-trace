f90b5c419153f00201cacae99735ec5058b9d921
congzw <2981810300@qq.com>
2019-08-28 17:39:09 +0800

    add api proxy

---
+++ src/SimpleTrace.Shared/TraceClients/ApiProxy/IClientTracerApiProxy.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SimpleTrace.TraceClients.Api;
+
+namespace SimpleTrace.TraceClients.ApiProxy
+{
+    public interface IClientTracerApiProxy : IClientTracerApi, ITestApi
+    {
+        Task<bool> TryTestApiConnection();
+    }
+}
