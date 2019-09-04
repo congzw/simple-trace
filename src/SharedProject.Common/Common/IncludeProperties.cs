69e8cad1fb67f8007e612b2003197281987d6c79
congzw <2981810300@qq.com>
2019-09-04 12:09:25 +0800

    add IncludeProperties

---
+++ src/SharedProject.Common/Common/IncludeProperties.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable CheckNamespace
+namespace Common
+{
+    public class IncludeProperties
+    {
+        public IncludeProperties()
+        {
+            SplitChar = ',';
+            Properties = string.Empty;
+            IgnoreCase = true;
+        }
+
+        public bool IgnoreCase { get; set; }
+        public char SplitChar { get; set; }
+        public string Properties { get; set; }
+
+        public IncludeProperties SetProperty(string name, bool include)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return this;
+            }
+
+            if (Properties == null)
+            {
+                Properties = string.Empty;
+            }
+
+            var names = Properties.Split(SplitChar).Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+            return include ? AddIfNotExit(names, name) : RemoveIfExit(names, name);
+        }
+
+        public IncludeProperties SetProperties(bool include, params string[] names)
+        {
+            foreach (var name in names)
+            {
+                SetProperty(name, include);
+            }
+            return this;
+        }
+
+        public bool HasProperty(string name)
+        {
+            if (string.IsNullOrWhiteSpace(Properties))
+            {
+                return false;
+            }
+            var names = Properties.Split(SplitChar).Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+            var tryFindIt = TryFindIt(names, name);
+            return !string.IsNullOrWhiteSpace(tryFindIt);
+        }
+
+        private string TryFindIt(IList<string> names, string name)
+        {
+            return IgnoreCase ? names.FirstOrDefault(x => x.Equals(name, StringComparison.OrdinalIgnoreCase)) : names.FirstOrDefault(x => x == name);
+        }
+        private IncludeProperties AddIfNotExit(IList<string> names, string name)
+        {
+            var theOne = TryFindIt(names, name);
+            if (string.IsNullOrWhiteSpace(theOne))
+            {
+                names.Add(name);
+                Properties = string.Join(SplitChar.ToString(), names);
+                return this;
+            }
+            return this;
+        }
+        private IncludeProperties RemoveIfExit(IList<string> names, string name)
+        {
+            var theOne = TryFindIt(names, name);
+            if (string.IsNullOrWhiteSpace(theOne))
+            {
+                return this;
+            }
+            names.Remove(theOne);
+            Properties = string.Join(SplitChar.ToString(), names);
+            return this;
+        }
+
+        public static IncludeProperties Create(bool ignoreCase = true, char splitChar = ',')
+        {
+            var includeProperties = new IncludeProperties();
+            includeProperties.IgnoreCase = ignoreCase;
+            includeProperties.SplitChar = splitChar;
+            return includeProperties;
+        }
+    }
+}
---
+++ src/SimpleTrace.UnitTest/Common/IncludePropertiesSpec.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable CheckNamespace
+
+namespace Common
+{
+    [TestClass]
+    public class IncludePropertiesSpec
+    {
+        [TestMethod]
+        public void SetProperties_Null_ShouldOk()
+        {
+            var includeProperties = IncludeProperties.Create().SetProperties(true, "A", "b", "", null);
+            includeProperties.Properties.Log().ShouldEqual("A,b");
+        }
+
+        [TestMethod]
+        public void Properties_Split_ShouldOk()
+        {
+            var includeProperties = IncludeProperties.Create(true, '|').SetProperties(true, "A", "b");
+            includeProperties.Properties.Log().ShouldEqual("A|b");
+
+            var includeProperties2 = IncludeProperties.Create(true).SetProperties(true, "A", "b");
+            includeProperties2.Properties.Log().ShouldEqual("A,b");
+        }
+
+        [TestMethod]
+        public void SetProperties_IgnoreCase_ShouldOk()
+        {
+            var includeProperties = IncludeProperties.Create(true).SetProperties(true, "A", "b");
+            includeProperties.Properties.Log();
+            includeProperties.HasProperty("A").ShouldTrue();
+            includeProperties.HasProperty("B").ShouldTrue();
+            includeProperties.HasProperty("C").ShouldFalse();
+        }
+
+        [TestMethod]
+        public void SetProperties_NotIgnoreCase_ShouldOk()
+        {
+            var includeProperties = IncludeProperties.Create(false).SetProperties(true, "A", "b");
+            includeProperties.Properties.Log();
+            includeProperties.HasProperty("A").ShouldTrue();
+            includeProperties.HasProperty("B").ShouldFalse();
+            includeProperties.HasProperty("C").ShouldFalse();
+        }
+    }
+}
