5af210a3d72a940e2893c7b24d22e7a2fa84e2d8
congzw <2981810300@qq.com>
2019-09-05 09:13:20 +0800

    add base dir log

--- src/SimpleTrace.Api/Program.cs
+++ src/SimpleTrace.Api/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Common;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace SimpleTrace.Api
 {
@@ -12,6 +15,12 @@ namespace SimpleTrace.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .ConfigureAppConfiguration(cfg => 
+                {
+                    //fix windows service base path system32 bugs!
+                    SimpleLogSingleton<Program>.Instance.Logger.LogInfo("BaseDirectory: " + AppDomain.CurrentDomain.BaseDirectory);
+                    cfg.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
+                })
+            .UseStartup<Startup>();
     }
 }
