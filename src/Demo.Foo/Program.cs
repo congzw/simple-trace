3afa4f09edff5bc66a165b40980c437cc92fd092
congzw <2981810300@qq.com>
2019-09-07 18:09:51 +0800

    add Demo.Foo

---
+++ src/Demo.Foo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo.Foo
+{
+    class Program
+    {
+        private static bool _looping = true;
+
+        static void Main(string[] args)
+        {
+            LogInfo("Use 'ESC' to Exit App");
+            Task.Run(() =>
+            {
+                while (_looping)
+                {
+                    LogInfo("...");
+                    Thread.Sleep(3000);
+                }
+            });
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                LogInfo("Use 'ESC' KEY to Exit App");
+                _looping = false;
+            }
+        }
+        
+        static void LogInfo(string info)
+        {
+            var msg = string.Format("[{0}][{1}][{2}] {3} => {4:s}", "SimpleLog", "Info", "Demo.Foo", info, DateTime.Now);
+            Console.WriteLine(msg);
+            Trace.WriteLine(msg);
+        }
+    }
+}
