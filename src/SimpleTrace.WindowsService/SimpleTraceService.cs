09fb15bb3088f75e4de01479863bfeb34d0f1821
congzw <2981810300@qq.com>
2019-09-07 15:57:35 +0800

    add process runner check

--- src/SimpleTrace.WindowsService/SimpleTraceService.cs
+++ src/SimpleTrace.WindowsService/SimpleTraceService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.ServiceProcess;
 using System.Threading;
 using Common;
@@ -10,19 +11,68 @@ namespace SimpleTrace.Ws
         public SimpleTraceService()
         {
             InitializeComponent();
+            LoopTask = new SimpleLoopTask();
+            Init(LoopTask);
         }
-        
+
+        private void Init(SimpleLoopTask loopTask)
+        {
+            var jaegerInfo = SimpleProcessInfo.Create("jaeger-all-in-one", "jaeger-all-in-one.exe", "--collector.zipkin.http-port=9411");
+            var jaegerProcess = SimpleProcess.GetOrCreate(jaegerInfo);
+            var jaegerRunner = new SimpleProcessRunner(jaegerProcess);
+
+            var traceApiInfo = SimpleProcessInfo.Create("Zonekey.EzTrace.Api", "Zonekey.EzTrace.Api.exe", null);
+            var traceApiProcess = SimpleProcess.GetOrCreate(traceApiInfo);
+            var traceApiRunner = new SimpleProcessRunner(traceApiProcess);
+
+            //update by config 
+            var simpleIniFile = SimpleIni.ResolveFile();
+
+            var fullPath = Path.GetFullPath(AppDomain.CurrentDomain.Combine("EzTrace.ini"));
+            var traceIniItems = simpleIniFile.TryLoadIniFileItems(fullPath);
+            LogInfo("EzTrace.ini => " + fullPath);
+
+            if (traceIniItems != null)
+            {
+                simpleIniFile.SetProperties(traceIniItems, jaegerInfo, "Jaeger");
+                simpleIniFile.SetProperties(traceIniItems, traceApiInfo, "TraceApi");
+            }
+            // ProcessName=jaeger-all-in-one;ExePath=Jaeger\jaeger-all-in-one.exe;ExeArgs=--collector.zipkin.http-port=9411 
+            LogInfo("----JaegerInfo----");
+            LogInfo(MyModelHelper.MakeIniString(jaegerInfo));
+            LogInfo("----TraceApiInfo----");
+            LogInfo(MyModelHelper.MakeIniString(traceApiInfo));
+
+            loopTask.LoopSpan = TimeSpan.FromSeconds(15);
+            loopTask.LoopAction = () =>
+            {
+                LogInfo("looping check trace service");
+                jaegerRunner.TryStart();
+                traceApiRunner.TryStart();
+
+            };
+
+            loopTask.AfterExitLoopAction = () =>
+            {
+                LogInfo("exiting check trace service");
+                jaegerRunner.TryStop();
+                traceApiRunner.TryStop();
+            };
+        }
+
+        public SimpleLoopTask LoopTask { get; set; }
+
         protected override void OnStart(string[] args)
         {
             LogInfo(string.Format("OnStart begin {0:yyyy-MM-dd HH:mm:ss:fff} in thread {1}", DateTime.Now, Thread.CurrentThread.ManagedThreadId));
-            //LoopTask.Start();
+            LoopTask.Start();
             LogInfo(string.Format("OnStart end {0:yyyy-MM-dd HH:mm:ss:fff} in thread {1}", DateTime.Now, Thread.CurrentThread.ManagedThreadId));
         }
 
         protected override void OnStop()
         {
             LogInfo(string.Format("OnStop begin {0:yyyy-MM-dd HH:mm:ss:fff} in thread {1}", DateTime.Now, Thread.CurrentThread.ManagedThreadId));
-            //LoopTask.Stop();
+            LoopTask.Stop();
             LogInfo(string.Format("OnStop end {0:yyyy-MM-dd HH:mm:ss:fff} in thread {1}", DateTime.Now, Thread.CurrentThread.ManagedThreadId));
         }
 
